ecc479b7665290c18054b598f11bfc955d051c75
patricio <pecheve.p@gmail.com>
2020-04-28 23:14:22 -0400

    respaldo

--- src/app/components/crea-cuenta/crea-cuenta.component.ts
+++ src/app/components/crea-cuenta/crea-cuenta.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-crea-cuenta',
   templateUrl: './crea-cuenta.component.html',
   styleUrls: ['./crea-cuenta.component.less']
 })
 export class CreaCuentaComponent implements OnInit {
-
-  constructor() { }
+  enviroment:boolean;
+  constructor() { 
+    this.enviroment = environment.production;
+  }
 
   ngOnInit() {
   }
